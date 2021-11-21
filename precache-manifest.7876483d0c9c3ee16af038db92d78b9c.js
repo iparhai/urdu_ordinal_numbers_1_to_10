0cb255721866e1423a102f2c5c9752060da01432
devshaffan <devshaffan@gmail.com>
2021-11-21 17:57:54 +0500

    Updates

---
+++ precache-manifest.7876483d0c9c3ee16af038db92d78b9c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d0b5dccf2088b3425f2",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/css/main.fec306c5.chunk.css"
+  },
+  {
+    "revision": "4d0b5dccf2088b3425f2",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/js/main.3c482dbf.chunk.js"
+  },
+  {
+    "revision": "3e6ceba2daa0ba5a2c65",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/js/runtime~main.90bfcb46.js"
+  },
+  {
+    "revision": "98794a35aefc76b542ef",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/css/2.6c1e483c.chunk.css"
+  },
+  {
+    "revision": "98794a35aefc76b542ef",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/js/2.962353b1.chunk.js"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "5f1c11259cb15bc3800a9bb43a4a7242",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/background.5f1c1125.gif"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "47df90f219856aa11addc7a1fdce1ea2",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/zero.47df90f2.png"
+  },
+  {
+    "revision": "3fda3c9da4ec590c5c88e647022a3170",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/one.3fda3c9d.png"
+  },
+  {
+    "revision": "5958b1ecfa6c8a21320db07c4c44221a",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/two.5958b1ec.png"
+  },
+  {
+    "revision": "9f699579df78dbbfbf2a4e1bff8b2ce0",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/three.9f699579.png"
+  },
+  {
+    "revision": "ad6cf83e9ded1abe81fb365462c8f4d7",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/four.ad6cf83e.png"
+  },
+  {
+    "revision": "eacd1490e0424542571b019369a01fe9",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/five.eacd1490.png"
+  },
+  {
+    "revision": "15ef5fbda60a9f2559b901ba1fae4485",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/six.15ef5fbd.png"
+  },
+  {
+    "revision": "612985c4d71cd2ef03008c8eaca76565",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/seven.612985c4.png"
+  },
+  {
+    "revision": "40b97ecd399aaa848af14e70689a2bed",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/eight.40b97ecd.png"
+  },
+  {
+    "revision": "6f04579f42ad580f201faf4ad35b0c81",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/nine.6f04579f.png"
+  },
+  {
+    "revision": "59843b2d65b09b144fd402b9327e37d1",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/instructions.59843b2d.gif"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/urdu_ordinal_numbers_1_to_10/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "49c37d2ddd44c26962d9c47ade2d16f1",
+    "url": "/urdu_ordinal_numbers_1_to_10/index.html"
+  }
+];
